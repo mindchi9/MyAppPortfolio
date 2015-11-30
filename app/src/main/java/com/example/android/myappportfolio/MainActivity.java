ec24e3036dd5f425e7751ecc03d55397eceb8e51
mindchi9 <mervjo2@gmail.com>
2015-11-29 20:38:25 -0500

    creating initial version of Udacity Project 0

---
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.android.myappportfolio;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+    /** Called when the user touches the button */
+    public void spotify_streamer(View view) {
+        // Do something in response to button click
+
+        Context context = getApplicationContext();
+        CharSequence text = "Streaming!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+
+
+
+    }
+
+    public void scores_app(View view) {
+        // Do something in response to button click
+
+        Context context = getApplicationContext();
+        CharSequence text = "Score !!!!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+
+
+
+    }
+
+
+    public void library_app(View view) {
+        // Do something in response to button click
+
+        Context context = getApplicationContext();
+        CharSequence text = "Reading is Fundamental!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+    }
+
+    public void build_it(View view) {
+        // Do something in response to button click
+
+        Context context = getApplicationContext();
+        CharSequence text = "Build It!!!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+
+    }
+    public void xyz_reader(View view) {
+        // Do something in response to button click
+
+        Context context = getApplicationContext();
+        CharSequence text = "Reading!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+    }
+
+
+
+    public void capstone(View view) {
+        // Do something in response to button click
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
